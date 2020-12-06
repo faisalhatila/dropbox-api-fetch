9b20dd86ad58897b1820c5ac284c13a5d3cde97b
Faisal Hatila <hatilas1994@gmail.com>
2020-12-07 01:11:27 +0500

    backend of dropbox api

---
+++ server.js
@@ -0,0 +1,45 @@
+const fetch = require('node-fetch');
+const app = require('express')();
+
+const hostname = 'localhost';
+const port = 3000;
+
+const config = {
+  fetch,
+  clientId: '2680u6b1md405q3',
+};
+
+const { Dropbox } = require('dropbox'); // eslint-disable-line import/no-unresolved
+
+const dbx = new Dropbox(config);
+
+const redirectUri = `http://${hostname}:${port}/auth`;
+const authUrl = dbx.auth.getAuthenticationUrl(redirectUri, null, 'code', 'offline', null, 'none', true);
+
+app.get('/', (req, res) => {
+  res.writeHead(302, { Location: authUrl });
+  res.end();
+});
+
+app.get('/auth', (req, res) => { // eslint-disable-line no-unused-vars
+  const { code } = req.query;
+  console.log(`code:${code}`);
+
+  dbx.auth.getAccessTokenFromCode(redirectUri, code)
+    .then((token) => {
+      console.log(`Token Result:${JSON.stringify(token)}`);
+      dbx.auth.setRefreshToken(token.refreshToken);
+      dbx.usersGetCurrentAccount()
+        .then((response) => {
+          console.log('response', response);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.listen(port);
